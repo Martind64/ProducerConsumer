bbc655edbfe6517fd5793c6bd74c914fc4a6a491
Martind64 <Martind_64@hotmail.com>
2014-12-07 15:31:09 +0100

    Addded Producer

---
+++ ProducerConsumer/Producer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProducerConsumer
+{
+    public class Producer
+    {
+        private readonly IBuffer _iBuffer;
+        private int _howMany;
+        private int _lastElement = -1;
+
+        
+
+        public Producer(IBuffer buffer, int howmany)
+        {
+            if (buffer == null){throw new ArgumentNullException("buffer");}
+            if (howmany > 0){throw new ArgumentOutOfRangeException("howmany", howmany, "howmany must be a positive number");}
+
+            _iBuffer = buffer;
+            _howMany = howmany;
+        }
+
+
+        public void Run()
+        {
+            for (int i = 0; i < _howMany; i++)
+            {
+                _iBuffer.Add(i);
+                Console.WriteLine("Producer Added" + i);
+            }
+            _iBuffer.Add(_lastElement);
+        }
+
+
+
+    }
+}
