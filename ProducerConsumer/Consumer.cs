153e3bd64533f0b4cf928629f75614f72c5d0dea
Martind64 <Martind_64@hotmail.com>
2014-12-07 16:14:49 +0100

    Added Consumer

---
+++ ProducerConsumer/Consumer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProducerConsumer
+{
+    public class Consumer
+    {
+        private IBuffer _iBuffer;
+        private int _howMany;
+
+        public Consumer(IBuffer ibuffer, int howMany)
+        {
+            _iBuffer = ibuffer;
+            _howMany = howMany;
+        }
+
+
+        public void Run()
+        {
+            while (true)
+            {
+                int element = _iBuffer.Take();
+                if (element == Producer._lastElement)
+                {
+                    break;
+                }
+                Console.WriteLine("Consumer Take" + element);
+            }
+            
+        }
+    }
+}
--- ProducerConsumer/Producer.cs
+++ ProducerConsumer/Producer.cs
@@ -9,32 +9,31 @@ namespace ProducerConsumer
 {
     public class Producer
     {
-        private readonly IBuffer _iBuffer;
+
+        private IBuffer _iBuffer;
         private int _howMany;
-        private int _lastElement = -1;
+        public static int _lastElement = -1;
 
-        
 
-        public Producer(IBuffer buffer, int howmany)
+        public Producer(IBuffer iBuffer, int howMany)
         {
-            if (buffer == null){throw new ArgumentNullException("buffer");}
-            if (howmany > 0){throw new ArgumentOutOfRangeException("howmany", howmany, "howmany must be a positive number");}
-
-            _iBuffer = buffer;
-            _howMany = howmany;
+            if (iBuffer == null){throw new ArgumentNullException("Buffer");}
+            if (howMany > 0){throw new ArgumentOutOfRangeException("howMany", howMany, "howMany must be a positive number");}
+            _iBuffer = iBuffer;
+            _howMany = howMany;
         }
-
 
         public void Run()
         {
+
             for (int i = 0; i < _howMany; i++)
             {
                 _iBuffer.Add(i);
                 Console.WriteLine("Producer Added" + i);
             }
+
             _iBuffer.Add(_lastElement);
         }
-
 
 
     }
